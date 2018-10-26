b2040b0fd23bf5fe291ecc4b060df10d8b3f380f
Alan_1990 <alanrodriguez721990@gmail.com>
2018-10-26 14:47:28 -0300

    Arreglo app.js

--- sw.js
+++ sw.js
@@ -25,7 +25,6 @@ const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    //no iria aca xq se puede modificar
     'css/animate.css',
     'js/libs/jquery.js'
 ];
