871bd3bcaa5031a86f63d769e988463ef05cff23
Alan_1990 <alanrodriguez721990@gmail.com>
2018-10-26 14:53:49 -0300

    Arreglo app.js

--- sw.js
+++ sw.js
@@ -31,12 +31,7 @@ self.addEventListener('install', e => {
     const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
         cache.addAll(APP_SHELL));
 
-    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
-        cache.addAll(APP_SHELL_INMUTABLE));
-
-
-
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+    e.waitUntil(cacheStatic);
 
 });
 
